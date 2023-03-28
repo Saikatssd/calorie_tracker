bf8c10b4fa1890ea460770ca22510e44fd5cb145
Saikat Sundar Das <saikatsundardas@gmail.com>
2023-03-28 13:05:08 +0530

    Updates

---
+++ precache-manifest.f7caa1093b1c20128fdd11ba15b8a064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd925c4c01072687e39d",
+    "url": "/fitness_tracker/static/css/main.414cf322.chunk.css"
+  },
+  {
+    "revision": "fd925c4c01072687e39d",
+    "url": "/fitness_tracker/static/js/main.fd925c4c.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/fitness_tracker/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "442f9bb09ebd334a668c",
+    "url": "/fitness_tracker/static/js/2.442f9bb0.chunk.js"
+  },
+  {
+    "revision": "c0da5008ed1709f3af0e",
+    "url": "/fitness_tracker/static/js/runtime~main.c0da5008.js"
+  },
+  {
+    "revision": "6bfd8773cbb30198b81da6273df6b2f8",
+    "url": "/fitness_tracker/static/media/calorie_tracker-logo.6bfd8773.png"
+  },
+  {
+    "revision": "97eceeaf0dde93685de03dc0a97c2f3a",
+    "url": "/fitness_tracker/index.html"
+  }
+];
